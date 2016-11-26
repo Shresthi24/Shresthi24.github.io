87240fd1bf3717122650ffe6a52ae13f1bdcb4b9
shresthi <shresthi94@gmail.com>
2016-11-26 15:48:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,75 +17,75 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('index-green.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index-green.html'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('index-gray.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index-gray.html'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('index-orange.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index-orange.html'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('tm-bg-slide-1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'tm-bg-slide-1.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('tm-bg-slide-2.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'tm-bg-slide-2.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('tm-bg-slide-3.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'tm-bg-slide-3.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('tm-easy-profile.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'tm-easy-profile.jpg'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('font-awesome.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'font-awesome.min.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('templatemo-blue.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'templatemo-blue.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('templatemo-gray.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'templatemo-gray.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('templatemo-orange.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'templatemo-orange.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('templatemo-green.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'templatemo-green.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('._font-awesome.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '._font-awesome.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('bootstrap.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('custom.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'custom.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('jquery.backstretch.min.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jquery.backstretch.min.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('jquery.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'jquery.js'));
 });
 
